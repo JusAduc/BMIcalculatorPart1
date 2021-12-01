7ccd940a388c3cde6f8d19297dac4f9312efe781
byaratch <yaratch_blake@student.mahoningctc.com>
2021-12-01 11:57:42 -0500

    file text, added fail safe

--- BMIcalculatorPart1/MainWindow.xaml.cs
+++ BMIcalculatorPart1/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace BMIcalculatorPart1
 {
@@ -33,12 +34,21 @@ namespace BMIcalculatorPart1
         }
         private void submitBtn_Click(object sender, RoutedEventArgs e)
         {
-            firstName = firstNameInput.Text;
-            lastName = lastNameInput.Text;
-            phoneNumber = phoneInput.Text;
-            height = Double.Parse(heightInput.Text);
-            weight = Double.Parse(weightInput.Text);
-            MessageBox.Show($"{firstName} {lastName} {phoneNumber} {height} {weight}");
+            //I dont need firstName, lastName and phone inputs for this right?
+            //if (firstNameInput.Text == "" || lastNameInput.Text == "" || phoneInput.Text == "")
+            if (heightInput.Text == ""|| weightInput.Text=="")
+            {
+                MessageBox.Show("This is a message box that appears if there was an error, please input information into each box.");
+            }
+            //Find out why this is giving an error. It will not work if it is added into the first if statement 
+            else {
+                firstName = firstNameInput.Text;
+                lastName = lastNameInput.Text;
+                phoneNumber = phoneInput.Text;
+                height = Double.Parse(heightInput.Text);
+                weight = Double.Parse(weightInput.Text);
+                MessageBox.Show($"{firstName} {lastName} {phoneNumber} {height} {weight}");
+            }
         }
         private void clearBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -54,5 +64,11 @@ namespace BMIcalculatorPart1
             Close();
         }
 
+        public void fileText()
+        {
+            string userInfo = $"Add all user info here:";
+            //Need to fix, ran out of time
+            File.WriteAllLines("information", userInfo);
+        }
     }
 }
